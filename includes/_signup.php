bd6559072c703069423f53888aa9def244ed0cbd
diwacreation3 <diwakarcreation@gmail.com>
2024-06-03 23:18:31 +0545

    Status: Signup system 🪻🍁

---
+++ includes/_signup.php
@@ -0,0 +1,126 @@
+<?php
+// Include config file 
+require_once "../config/db_config.php";
+
+// Define variable and initilize with empty values 
+$email = $username = $password = $confirm_password = "";
+$email_err = $username_err = $password_err = $confirm_password_err = "";
+
+// processing form data when fomr is submitted 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // validate username 
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter a username.";
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
+        $username_err = "Username can only contain letters, numbers and underscores ";
+    } else {
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+
+        if ($stmt = $mysqli->prepare($sql)) {
+            // bind variables to rhe prepared statement as parameters 
+            $stmt->bind_param("s", $param_username);
+
+            // Set parameters 
+            $param_username = trim($_POST["username"]);
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // store result
+                $stmt->store_result();
+
+                if ($stmt->num_rows == 1) {
+                    $username_err = "This username already taken";
+                } else {
+                    $username = trim($_POST["username"]);
+                }
+            } else {
+                echo "Opps! Something went wrong. Please try again later.";
+            }
+            //close statement
+            $stmt->close();
+        }
+    }
+
+    // validate email
+    if (empty(trim($_POST["email"]))) {
+        $email_err = "Please enter email";
+    } else {
+        // prepare the select information 
+        $sql = "SELECT id FROM users WHERE email = ? ";
+
+        if ($stmt = $mysqli->prepare($sql)) {
+            // bind variable as paramter 
+            $stmt->bind_param("s", $param_email);
+
+            // set parameter
+            $param_email = trim($_POST["email"]);
+
+
+            // attempt to execute to prepare statements 
+            if ($stmt->execute()) {
+                // store result 
+                $stmt->store_result();
+                if ($stmt->num_rows == 1) {
+                    $email_err = "Email already in use";
+                } else {
+                    $email = trim($_POST["email"]);
+                }
+            } else {
+                echo "Opps! something went wrong. please try again later";
+            }
+            $stmt->close();
+        }
+    }
+
+    // Validate password
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have atleast 6 characters  ";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate confim password 
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password. ";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($password_err) && ($password != $confirm_password)) {
+            $confirm_password_err = "Password did not match";
+        }
+    }
+
+    // Check input errors before inserting in database 
+    if (empty($username_err) && empty($email_err) && empty($confirm_password_err) && empty($confirm_password_err)) {
+        // prepare an insert statement 
+        $sql = "INSERT INTO users ( email ,username, password) VALUES (?,?,?)";
+
+        if ($stmt = $mysqli->prepare($sql)) {
+            // Bind variable to the prepared statement parameters 
+            $stmt->bind_param("sss", $param_email, $param_username, $param_password);
+
+            // set email parameter
+            $param_email = $email;
+            // set parameters 
+            $param_username = $username;
+            //create password hash
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Attempt to execute the prepared statement 
+
+            if ($stmt->execute()) {
+                // Rediect to login page 
+                header("Location: login.php");
+            } else {
+                echo "Opps! something went wrong. please try again later ";
+            }
+            $stmt->close();
+        }
+    }
+    // close connection
+    $mysqli->close();
+}
+?>
---
+++ pages/signup.php
@@ -0,0 +1,57 @@
+<?php
+require "../includes/_signup.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Game Store | Signup</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+  <div class="container">
+    <div class="card signup-card">
+      <img src="../assets/logo.png" alt="Company Logo" class="img-fluid mb-2 d-block mx-auto" height="60px" width="60px">
+
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text"  id="username" name="username" placeholder="Enter username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+          <span class="invalid-feedback"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group">
+          <label for="email">Email Address</label>
+          <input type="email"  id="email" name="email" placeholder="Enter email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+          <span class="invalid-feedback"><?php echo $confirm_email_err; ?></span>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password"  id="password" name="password" placeholder="Enter password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+          <span class="invalid-feedback"><?php echo $password_err; ?></span>
+          <!-- <span class="input-group-append flex">
+                  <!-- <button id="togglePassword" type="button" class="btn btn-outline-secondary">
+                    <i class="bi bi-eye-slash"></i>
+                  </button> --
+                  <input type="checkbox" id="togglepassword" name="toggle" > show Password <br>
+                </span>  -->
+        </div>
+        <div class="form-group">
+          <label for="password">Confirm Password</label>
+          <input type="password"  id="confirm-password" name="confirm_password" placeholder="Re-enter your password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+          <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+        </div>
+        
+        <br>
+        <button type="submit" name="submit" class="btn btn-primary">Sign Up</button> <br>
+        Already have an account <a href="login.html">Login</a>
+      </form>
+    </div>
+  </div>
+<script src="../scripts/main.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
